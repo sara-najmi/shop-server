2237a36212c91c691d66540080c11cb61ab66c15
Moh3n75 <42858830+Moh3n75@users.noreply.github.com>
2024-01-08 18:15:01 +0330

    product image

--- src/main/java/com/najmi/shop/vendor/VendorServiceImpl.java
+++ src/main/java/com/najmi/shop/vendor/VendorServiceImpl.java
@@ -31,9 +31,28 @@ public class VendorServiceImpl implements VendorService {
     @Override
     @Transactional(rollbackFor = Exception.class)
     public void fetch() throws Exception {
+
+        String[] userAgents = {
+                "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
+                "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Edge/91.0.864.59 Safari/537.36",
+                "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Firefox/89.0",
+                "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Safari/14.1.1",
+                "Mozilla/5.0 (iPhone; CPU iPhone OS 14_6 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.1.1 Mobile/15E148 Safari/604.1",
+                "Mozilla/5.0 (Android 11; Mobile; rv:68.0) Gecko/68.0 Firefox/68.0",
+                "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36 Edg/91.0.864.59",
+                "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36 OPR/77.0.4054.277",
+                "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36 Vivaldi/4.1.2369.21",
+                "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36 Brave/1.26.74",
+                "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36 YaBrowser/21.6.3.757 Yowser/2.5 Safari/537.36",
+                "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36 DuckDuckGo/5",
+                // Add more user agents as needed
+        };
+
+
+
         String url = BASE_URL + "/marketplace/storeslist/tehran/electronic-devices";
         Document document = Jsoup.connect(url)
-                .userAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36")
+                .userAgent(userAgents[(int) (Math.random() * userAgents.length)])
                 .get();
         Elements links = document.select("a[href*=marketplace]");
         for (Element link : links) {
@@ -43,7 +62,7 @@ public class VendorServiceImpl implements VendorService {
             vendor.setAddress(link.select(".kt-event-row__text--subtitle").text());
             vendorRepository.save(vendor);
             Document documentProduct = Jsoup.connect(BASE_URL + link.select("a[href]").attr("href"))
-                    .userAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36")
+                    .userAgent(userAgents[(int) (Math.random() * userAgents.length)])
                     .get();
             Elements productLinks = documentProduct.select("a[href*=/v]");
             for (Element productLink : productLinks) {
